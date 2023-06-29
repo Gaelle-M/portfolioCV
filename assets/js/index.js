4e6189c1d224cce3b5ee5a5fb73552a1b2b5f412
Gaelle-M <gaelle-mazzali@hotmail.fr>
2023-06-29 18:28:08 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+//Effet du body
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  tablet: { smooth: true },
+  smartphone: { smooth: true },
+});
+
+//Effet de "à propos"
+const target = document.getElementById("target");
+let array = ["Créative", "Dynamique", "Autonome", "Passionnée", "Organisée", "Rigoureuse", "Réactive"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+// Body color
+scroll.on("scroll", () => {
+  if (document.querySelector("#color.is-inview")) {
+    document.body.style.background = "#161616ed";
+    document.body.style.color = "#fefeff";
+  } else {
+    document.body.style.background = "#fefeffe6";
+    document.body.style.color = "#161616ed";
+  }
+});
+
+//text anim
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.textContent = array[wordIndex][letterIndex];
+
+  setTimeout(() => {
+    letter.remove();
+  }, 1200);
+};
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+    } else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 1200);
+    }
+  }, 60);
+};
+
+loop();
+
+
